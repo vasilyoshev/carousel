823e179d5157b0eff34bdc119910b086d7d6a8c3
Vasil Yoshev <vasil.yoshev@gmail.com>
2025-08-26 11:37:05 +0300

    Setup tests

---
+++ src/App.spec.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('App component', () => {
+  it('renders', () => {
+    render(<App />);
+    expect(screen.getByText(/Vite \+ React/i)).toBeInTheDocument();
+  });
+});
